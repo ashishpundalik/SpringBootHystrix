9c504e6986bb099fb34b5caa081ebcc3b649cfb2
Aashish Pundalik <pundalikaashish@gmail.com>
2018-04-17 10:41:58 +0800

    | Aashish | Added Hystrix configuration.

--- src/main/java/starter/Application.java
+++ src/main/java/starter/Application.java
@@ -1,13 +1,17 @@
 package starter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 /**
  * Created by ashishpundalik on 13/06/2017.
  */
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableCircuitBreaker
 public class Application {
 
     public static void main(String[] args) {
